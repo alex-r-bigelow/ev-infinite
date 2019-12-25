68a3843e04f476bc5d84ac097aa7756f870130a1
Alex Bigelow <alex.r.bigelow@gmail.com>
2019-12-25 12:22:21 -0700

    Refine map view

--- views/MapView/MapView.js
+++ views/MapView/MapView.js
@@ -12,12 +12,16 @@ class MapView extends Modal {
     super.setup();
     this.d3el.html(this.resources[1]);
 
+    this.currentTransform = d3.zoomIdentity;
+
     this.d3el.select('svg')
       .call(d3.zoom()
         .on('zoom', () => {
+          this.currentTransform = d3.event.transform;
           this.d3el.selectAll('.systems, .links')
             .attr('transform', d3.event.transform);
-        }));
+        })
+        .on('end', () => { this.render(); }));
 
     this.d3el.select('.ok.button')
       .on('click', () => { window.controller.resume(); });
@@ -29,6 +33,19 @@ class MapView extends Modal {
       .attr('width', width)
       .attr('height', height);
 
+    // 200px per cell, centered at 0,0
+    this.xScale = d3.scaleLinear()
+      .domain([-1, 1])
+      .range([width / 2 - 200, width / 2 + 200]);
+    this.yScale = d3.scaleLinear()
+      .domain([-1, 1])
+      .range([height / 2 - 200, height / 2 + 200]);
+
+    // TODO: draw pre-rendered galactic textures, etc; don't bother rendering
+    // individual systems beyond this.currentTransform.k < 0.1 or so
+    this.drawSystems();
+  }
+  drawSystems () {
     // For now, just use the full radius of the universe (we're keeping
     // it small-ish enough to fit in memory / draw completely)
     const r = window.controller.universe.radius;
@@ -38,14 +55,6 @@ class MapView extends Modal {
       right: r,
       bottom: r
     });
-
-    // 200px per cell, centered at 0,0
-    const xScale = d3.scaleLinear()
-      .domain([-1, 1])
-      .range([width / 2 - 200, width / 2 + 200]);
-    const yScale = d3.scaleLinear()
-      .domain([-1, 1])
-      .range([height / 2 - 200, height / 2 + 200]);
 
     let systems = this.d3el.select('.systems').selectAll('.system')
       .data(graph.nodes, d => d.id);
@@ -59,11 +68,12 @@ class MapView extends Modal {
       .attr('text-anchor', 'middle')
       .attr('y', 15);
     systems.select('text')
-      .text(d => d.details.name);
+      .text(d => d.details.name)
+      .style('display', this.currentTransform.k < 0.75 ? 'none' : null);
 
     systems.attr('transform', d => {
-      const x = xScale(d.coordinates.x);
-      const y = yScale(d.coordinates.y);
+      const x = this.xScale(d.coordinates.x);
+      const y = this.yScale(d.coordinates.y);
       return `translate(${x},${y})`;
     });
 
@@ -76,10 +86,10 @@ class MapView extends Modal {
     linksEnter.append('line');
 
     links.select('line')
-      .attr('x1', d => xScale(d.source.coordinates.x))
-      .attr('y1', d => yScale(d.source.coordinates.y))
-      .attr('x2', d => xScale(d.target.coordinates.x))
-      .attr('y2', d => yScale(d.target.coordinates.y));
+      .attr('x1', d => this.xScale(d.source.coordinates.x))
+      .attr('y1', d => this.yScale(d.source.coordinates.y))
+      .attr('x2', d => this.xScale(d.target.coordinates.x))
+      .attr('y2', d => this.yScale(d.target.coordinates.y));
   }
 }
 export default MapView;
